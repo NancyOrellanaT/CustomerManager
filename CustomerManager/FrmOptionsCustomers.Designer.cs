d76b0176d6667ab35c972e90162a4f16bacd1754
NancyOrellanaT <adrianaorellana34@gmail.com>
2019-01-30 00:32:03 -0400

    Cambios menores

--- CustomerManager/FrmOptionsCustomers.Designer.cs
+++ CustomerManager/FrmOptionsCustomers.Designer.cs
@@ -88,23 +88,6 @@
             this.label2.Size = new System.Drawing.Size(66, 25);
             this.label2.TabIndex = 7;
             this.label2.Text = "Menu";
-<<<<<<< HEAD
-            // 
-            // label1
-            // 
-            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label1.Location = new System.Drawing.Point(115, 669);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(144, 17);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "Customer Manager";
-=======
->>>>>>> 267a8fdb86622ea35431398e82d67c56e4c3a593
             // 
             // pictureBox1
             // 
